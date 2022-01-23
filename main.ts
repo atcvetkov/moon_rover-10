7ae8a72e4e9872d4e4a9a4eab700cc9f7c9daae6
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2022-01-23 21:00:16 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,30 @@
 led.enable(false)
 irRemote.connectInfrared(DigitalPin.P9)
-let p0_state = 0
 pins.servoWritePin(AnalogPin.P0, 90)
-let p4_status = 0
 pins.servoWritePin(AnalogPin.P4, 135)
+let p0p4_status = 0
 basic.forever(function () {
     if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Number_1)) {
-        if (p0_state == 0 && p4_status == 1) {
-            for (let index = 0; index <= 90; index++) {
-                pins.servoWritePin(AnalogPin.P0, index)
-                basic.pause(10)
-            }
-            p0_state = 1
-        } else if (p0_state == 1 && p4_status == 1) {
-            for (let index = 0; index <= 90; index++) {
-                pins.servoWritePin(AnalogPin.P0, 90 - index)
-                basic.pause(10)
-            }
-            p0_state = 0
-        }
-    }
-    if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Number_2)) {
-        if (p4_status == 0 && p0_state == 0) {
-            for (let index = 0; index <= 135; index++) {
-                pins.servoWritePin(AnalogPin.P4, index)
-                basic.pause(10)
-            }
-            p4_status = 1
-        } else if (p4_status == 1 && p0_state == 0) {
+        if (p0p4_status == 0) {
             for (let index = 0; index <= 135; index++) {
                 pins.servoWritePin(AnalogPin.P4, 135 - index)
                 basic.pause(10)
             }
-            p4_status = 0
+            for (let index = 0; index <= 90; index++) {
+                pins.servoWritePin(AnalogPin.P0, 90 - index)
+                basic.pause(10)
+            }
+            p0p4_status = 1
+        } else {
+            for (let index = 0; index <= 90; index++) {
+                pins.servoWritePin(AnalogPin.P0, index)
+                basic.pause(10)
+            }
+            for (let index = 0; index <= 135; index++) {
+                pins.servoWritePin(AnalogPin.P4, index)
+                basic.pause(10)
+            }
+            p0p4_status = 0
         }
     }
 })
